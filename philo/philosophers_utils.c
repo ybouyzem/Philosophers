a2864d5d0e9c042c594e6d4e508573982f23f56f
Younes Bouyzem <ybouyzem@e3r7p1.1337.ma>
2024-11-20 21:30:09 +0100

    .

--- philo/philosophers_utils.c
+++ philo/philosophers_utils.c
@@ -6,7 +6,7 @@
 /*   By: ybouyzem <ybouyzem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 12:05:30 by ybouyzem          #+#    #+#             */
-/*   Updated: 2024/11/17 18:13:43 by ybouyzem         ###   ########.fr       */
+/*   Updated: 2024/11/20 21:29:32 by ybouyzem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,8 +91,13 @@ void my_sleep(size_t time, t_philo *philo)
 	start = get_current_time();
 	while (get_current_time() - start < time)
 	{
+		pthread_mutex_lock(&philo->program->monitor_lock);
 		if (philo->program->program_finished)
-			break ;
+		{
+			pthread_mutex_unlock(&philo->program->monitor_lock);
+			return ;
+		}
+		pthread_mutex_unlock(&philo->program->monitor_lock);
 		usleep(100);
 	}
 }
