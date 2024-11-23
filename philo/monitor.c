a3750f3ab82e7224eb1c7f63666df11a3403a5a6
Younes Bouyzem <ybouyzem@e3r1p16.1337.ma>
2024-11-23 18:52:06 +0100

    done

--- philo/monitor.c
+++ philo/monitor.c
@@ -6,7 +6,7 @@
 /*   By: ybouyzem <ybouyzem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 06:29:51 by ybouyzem          #+#    #+#             */
-/*   Updated: 2024/11/23 03:57:42 by ybouyzem         ###   ########.fr       */
+/*   Updated: 2024/11/23 18:51:34 by ybouyzem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ int	all_philos_finished(t_philo *philos)
 void	*ft_monitor(void *arg)
 {
 	int		i;
-	size_t	passtime;
 	t_philo	*philos;
 
 	philos = (t_philo *)arg;
@@ -44,15 +43,15 @@ void	*ft_monitor(void *arg)
 		while (++i < philos[0].program->num_of_philos)
 		{
 			pthread_mutex_lock(&philos[0].program->monitor_lock);
-			passtime = get_current_time() - philos[i].last_meal;
-			if (passtime > philos[i].program->time_to_die
-				|| all_philos_finished(philos))
+			if ((get_current_time() - philos[i].last_meal)
+				> philos[i].program->time_to_die || all_philos_finished(philos))
 			{
 				pthread_mutex_lock(&philos[i].program->dead_lock);
 				philos->program->program_finished = 1;
 				pthread_mutex_unlock(&philos[i].program->dead_lock);
 				if (!all_philos_finished(philos))
-					printf("%zu %d died\n", (get_current_time() - philos[i].start_time), philos[i].id);
+					printf("%zu %d died\n", (get_current_time()
+							- philos[i].start_time), philos[i].id);
 				pthread_mutex_unlock(&philos[0].program->monitor_lock);
 				return (NULL);
 			}
